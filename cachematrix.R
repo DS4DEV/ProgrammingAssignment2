ba592fd9bcb3ac99922072d29373e986c16dbe6e
pauwelsb1@gmail.com <brampauwels@BramPauwels.local>
2018-09-16 16:15:31 +0200

    Commit: 2 new functions (paired)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions in pair caching the inverse of a matrix
+## Datascience Coursera - RProgramming - Week 3 Assignment
 
-## Write a short comment describing this function
+## Creates a special matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Initialize the inverse property
+  inverse <- NULL
+  
+  ## Method to set the matrix
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  
+  ## Method the get the matrix 
+  get <- function() x
+  
+  ## Method to get the inverse of the matrix
+  setinv <- function(inv) inverse <<- inv
+  getinv <- function() inverse
+  
+  ## Return a list of the methods
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Computes the inverse of the matrix returned by "makeCacheMatrix"
+## The "cachesolve" should retrieve the inverse from the cache, if the inverse has already been calculated
+
+cacheSolve <- function(x) {
+    ## Return a matrix that is the inverse of 'x'  
+  inverse <- x$getinv()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- solve(data)
+  x$setinv(inverse)
+  inverse
 }
+        
